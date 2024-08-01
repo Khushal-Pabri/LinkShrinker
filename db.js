0025eefd97462ddf60f452b2f66ac26984677756
Khushal-Pabri <pabrikhushal@gmail.com>
2024-08-01 22:07:19 +0530

    db connection changes

--- db.js
+++ db.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mydatabase');
+mongoose.connect('mongodb+srv://khushal:Khush%4015@cluster0.jjdko6m.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0');
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Mongoose connected to the database');
+});
+
+db.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+db.on('disconnected', () => {
+  console.log('Mongoose disconnected from the database');
+});
